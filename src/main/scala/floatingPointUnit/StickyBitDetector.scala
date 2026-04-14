7c4cf8d62bc6227cd052380f4f6a5db1463e4c81
Sebastian <sebastian@familienholdt.dk>
2026-04-14 20:03:11 +0200

    Cleanup StickyBitDetector

--- src/main/scala/floatingPointUnit/StickyBitDetector.scala
+++ src/main/scala/floatingPointUnit/StickyBitDetector.scala
@@ -15,24 +15,10 @@ class StickyBitDetector(size: Int) extends Module {
     io.sticky := io.input(0) && io.shift(0) 
   } else {
     val lowerSize = pow(2, (log2Up(size) - 1)).intValue
-    val higherSize = size - lowerSize
-
     val subDetector = Module(new StickyBitDetector(lowerSize))
-
-    when (io.shift(log2Up(size) - 1)) {
-      // When highest bit of shift is set
-      // lowest half of input should be or'ed
-      // remaining bits of shift should be tested on highest half
-      subDetector.io.input := io.input(size - 1, lowerSize)
-      subDetector.io.shift := io.shift(log2Up(size) - 2, 0)
-      io.sticky := subDetector.io.sticky || io.input(lowerSize - 1,0).orR 
-    } .otherwise {
-      // When highest bit of shift is not set
-      // highest half is ignored
-      // remaining bits of shift should be tested on lowest half
-      subDetector.io.input := io.input(lowerSize - 1, 0)
-      subDetector.io.shift := io.shift(log2Up(size) - 2, 0)
-      io.sticky := subDetector.io.sticky
-    }
+    val highBitOfShift = io.shift(log2Up(size) - 1)
+    subDetector.io.input := Mux(highBitOfShift, io.input(size - 1, lowerSize), io.input(lowerSize - 1, 0))
+    subDetector.io.shift := io.shift(log2Up(size) - 2, 0)
+    io.sticky := subDetector.io.sticky || highBitOfShift && io.input(lowerSize - 1, 0).orR
   }
 }
