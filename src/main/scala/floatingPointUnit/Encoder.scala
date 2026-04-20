0284b3c0d96e908a7107c77351d10a943434877a
Sebastian <sebastian@familienholdt.dk>
2026-04-20 20:46:39 +0200

    Fix typo in Encoder.scala

--- src/main/scala/floatingPointUnit/Encoder.scala
+++ src/main/scala/floatingPointUnit/Encoder.scala
@@ -13,8 +13,7 @@ class Encoder(exponentWidth: Int, mantissaWidth: Int) extends Module {
   })
 
   val denormal = !io.input.significand(significandWidth - 1)
-  val exponent = Mux(denormal, 0.U, io.input.exponent)
-  val normal = io.input.sign ## Mux(denormal, 0.U, exponent) ## io.input.significand(significandWidth - 2,0) // TODO: subtract bias
+  val normal = io.input.sign ## Mux(denormal, 0.U, io.input.exponent) ## io.input.significand(significandWidth - 2,0) // TODO: subtract bias
   val nan = 0.U(1.W) ## (pow(2, exponentWidth).intValue - 1).U(exponentWidth.W) ## 1.U(1.W) ## 0.U((mantissaWidth - 1).W)
   val infinity = io.input.sign ## (pow(2, exponentWidth).intValue - 1).U(exponentWidth.W) ## 0.U(mantissaWidth.W)
   io.output := Mux(io.input.nan, nan, Mux(io.input.infinity, infinity, normal))
