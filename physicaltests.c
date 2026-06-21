02a59902b2e5e3ca7a92d6e06ab38f7f0b9c69cf
Sebastian <sebastian@familienholdt.dk>
2026-06-21 11:34:37 +0200

    change numbers in physical test

--- physicaltests.c
+++ physicaltests.c
@@ -6,18 +6,17 @@ int asInt(float f) {
 }
 
 void main() {
-  float pi = 3.142857;
-  float e = 2.71828;
+  float pi = 3.1415927;
+  float e = 2.7182817;
   printf("Physical tests to be verified on fpuTester.FpuTester chisel module\n");
   printf("Tests include all operations and different rounding modes causing round up and round down\n");
-  printf("operand1: %f, operand2: %f\n", pi, e);
-  fesetround(FE_TONEAREST);
-  float result = pi + e;
-  printf("round nearest: 0x%08x + 0x%08x = 0x%08x (%.7f)\n", asInt(pi), asInt(e), asInt(result), result);
+  printf("operand1: %.7f, operand2: %.7f\n", pi, e);
   fesetround(FE_DOWNWARD);
-  result = pi + e;
+  float result = pi + e;
   printf("round down:    0x%08x + 0x%08x = 0x%08x (%.7f)\n", asInt(pi), asInt(e), asInt(result), result);
   fesetround(FE_TONEAREST);
+  result = pi + e;
+  printf("round nearest: 0x%08x + 0x%08x = 0x%08x (%.7f)\n", asInt(pi), asInt(e), asInt(result), result);
   result = pi - e;
   printf("round nearest: 0x%08x - 0x%08x = 0x%08x (%.7f)\n", asInt(pi), asInt(e), asInt(result), result);
   result = pi * e;
